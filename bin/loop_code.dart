d61dcb6ca6b9ac1eb39902937304244d92fa2432
Jahangir Alam <jahangir19935@gmail.com>
2020-10-28 05:07:07 +0600

    code for loop

---
+++ bin/loop_code.dart
@@ -0,0 +1,5 @@
+import 'package:loop_code/loop_code.dart' as loop_code;
+
+void main(List<String> arguments) {
+  print('Hello world: ${loop_code.calculate()}!');
+}
---
+++ test/loop_code_test.dart
@@ -0,0 +1,8 @@
+import 'package:loop_code/loop_code.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
